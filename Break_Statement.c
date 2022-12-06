aaad82372e5d08c4617efe18b2a1e7ce881dcd12
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2022-12-06 05:31:20 -0800

    Add files via upload

---
+++ Break_Statement.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int i = 0;
+    while (1)
+    {
+        if (i > 5)
+        {
+            break;
+        }
+        printf("%d ", i);
+        i++;
+    }
+ 
+    return 0;
+}
