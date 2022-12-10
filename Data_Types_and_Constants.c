67473808d9ba74d094d51e759af55d30647afb7e
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2022-12-10 02:11:32 -0800

    Add files via upload

---
+++ Data_Types_and_Constants.c
@@ -0,0 +1,24 @@
+/*
+#include <stdio.h>
+ 
+int main()
+{
+    // Creating variables having different data types
+    int integer = 26;
+    float floating = 39.32;
+    char character = 'A';
+ 
+    // Printing variables with the help of their respective format specifiers
+    printf("%d\n", integer);
+    printf("%f\n", floating);
+    printf("%c\n", character);
+}
+
+*/
+
+#include <stdio.h>
+ 
+int main()
+{
+    const int MOD = 10000007;
+}
