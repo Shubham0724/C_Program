acc5c8b2fc082361c450e136eac4055071fcbd71
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2022-12-24 05:14:20 -0800

    Add files via upload

---
+++ Operators.c
@@ -0,0 +1,50 @@
+// Arithmetic Operators
+/*
+#include <stdio.h>
+ 
+int main()
+{
+    int a = 2;
+    int b = 3;
+    printf("a + b = %d\n", a + b);
+}
+
+
+
+//Relational Operators
+#include <stdio.h>
+ 
+int main()
+{
+    int a = 2;
+    int b = 3;
+    printf("a == b = %d\n", a == b);
+}
+
+
+
+//Logical Operators
+
+#include <stdio.h>
+ 
+int main()
+{
+    int a = 1;
+    int b = 0;
+    printf("a or b = %d\n", a || b);
+}
+
+*/
+
+//Bitwise Operators
+
+#include <stdio.h>
+ 
+int main()
+{
+    int a = 2; //10
+    int b = 3; //11
+    printf("a xor b = %d\n", a ^ b);
+}
+
+
