9a61ab4faa4d5fbcf16e6a74e6986aabcd8aa70d
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2022-12-25 05:05:45 -0800

    Add files via upload

---
+++ pointers.c
@@ -0,0 +1,27 @@
+// operation on pointers
+
+// 1) Address of operators(&):
+
+
+#include <stdio.h>
+ 
+int main()
+{
+    int a = 100;
+    printf("%d\n", a);
+    printf("Address of variable a is %d", &a);
+    return 0;
+}
+
+/*
+// 2) Indirection operator(*):
+#include <stdio.h>
+ 
+int main()
+{
+    int a = 100;
+    printf("%d\n", a);
+    printf("Address of variable a is %d", &a);
+    return 0;
+}
+*/
